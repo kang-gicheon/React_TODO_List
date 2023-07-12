6cd40cee4a615e1eb4d45f7ca68f24b959479c4c
kanggicheon <snoopy1516@naver.com>
2023-07-12 16:52:38 +0900

    css 수정, component 구조 잡기
    최상위 컴포넌트 구조잡기,  CSS 수정

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* 헤더 영역 */}
+      <div>Header</div>
+      {/* 할 일을 작성할 edit 영역 */}
+      <div>Todo Editor</div>
+      {/* 작성한 할 일 목록 영역 */}
+      <div>Todo List</div>
+    </div>
+  );
 }
 
 export default App;
