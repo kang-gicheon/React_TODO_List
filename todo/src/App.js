33c2de99dd05322d9cdef6a038fc2798406139cc
kanggicheon <snoopy1516@naver.com>
2023-07-12 17:00:11 +0900

    Header component create, import
    헤더 영역 생성, app.js에 import 시키기

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
       {/* 헤더 영역 */}
-      <div>Header</div>
+      <Header />
       {/* 할 일을 작성할 edit 영역 */}
       <div>Todo Editor</div>
       {/* 작성한 할 일 목록 영역 */}
---
+++ todo/src/component/Header.js
@@ -0,0 +1,4 @@
+const Header = () => {
+  return <div className="Header">Component</div>;
+};
+export default Header;
