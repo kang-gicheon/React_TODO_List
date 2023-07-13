4b1341eb1a3bfd30619b1cd47d823dc5abe35b53
kanggicheon <snoopy1516@naver.com>
2023-07-13 09:26:40 +0900

    TodoList js, css 추가
    할 일 아이템을 표시할 컴포넌트 생성

--- todo/src/component/TodoList.js
+++ todo/src/component/TodoList.js
@@ -1,7 +1,12 @@
-import "./TodoEditor.css";
+import "./TodoList.css";
 
 const TodoList = () => {
-  return <div className="TodoList">TodoList component</div>;
+  return (
+    <div className="TodoList">
+      <h4>Todo List</h4>
+      <input className="searchbar" placeholder="검색어를 입력하세요" />
+    </div>
+  );
 };
 
 export default TodoList;
