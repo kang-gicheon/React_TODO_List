3990742658820b438e28b220ad1ce82379230389
kanggicheon <snoopy1516@naver.com>
2023-07-13 16:48:28 +0900

    TodoItem -> TodoList 컴포넌트 배치
    TodoItem import, 컴포넌트 배치 테스트

--- todo/src/component/TodoList.js
+++ todo/src/component/TodoList.js
@@ -1,3 +1,4 @@
+import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = () => {
@@ -5,6 +6,12 @@ const TodoList = () => {
     <div className="TodoList">
       <h4>Todo List</h4>
       <input className="searchbar" placeholder="검색어를 입력하세요" />
+      {/* 여러 개의 할 일 아이템을 리스트로 보여줄 div 태그 요소 배치 */}
+      <div className="list_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
     </div>
   );
 };
