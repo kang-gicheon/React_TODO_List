9e627405e36de9ce2e5398804f17e9767db8d32a
kanggicheon <snoopy1516@naver.com>
2023-07-13 09:04:58 +0900

    Todo List 컴포넌트 생성
    + app.js에 input 시키기

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
+import TodoList from "./component/TodoList";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       {/* 할 일을 작성할 edit 영역 */}
       <TodoEditor />
       {/* 작성한 할 일 목록 영역 */}
-      <div>Todo List</div>
+      <TodoList />
     </div>
   );
 }
---
+++ todo/src/component/TodoList.js
@@ -0,0 +1,7 @@
+import "./TodoEditor.css";
+
+const TodoList = () => {
+  return <div className="TodoList">TodoList component</div>;
+};
+
+export default TodoList;
