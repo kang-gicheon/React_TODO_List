9e5d34b5aea2604b1ba61baca16860bfc61cc568
kanggicheon <snoopy1516@naver.com>
2023-07-13 09:00:29 +0900

    TodoList 구조 만들기 + CSS 작성

--- todo/src/component/TodoEditor.js
+++ todo/src/component/TodoEditor.js
@@ -1,6 +1,14 @@
 import "./TodoEditor.css";
 
 const TodoEditor = () => {
-  return <div className="TodoEditor">TodoEditor Component</div>;
+  return (
+    <div className="TodoEditor">
+      <h4>새로운 Todo 작성하기 ✏</h4>
+      <div className="editor_wrapper">
+        <input placeholder="새로운 Todo" />
+        <button>추가하기</button>
+      </div>
+    </div>
+  );
 };
 export default TodoEditor;
