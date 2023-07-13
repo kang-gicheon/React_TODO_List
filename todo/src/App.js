c3e1a2222edf8c1691b9a81116732bf2cd02faf0
kanggicheon <snoopy1516@naver.com>
2023-07-13 17:41:18 +0900

    추가 함수 호출 app.js에 적용
    TodoEditor 컴포넌트 수정

--- todo/src/App.js
+++ todo/src/App.js
@@ -51,7 +51,7 @@ function App() {
       {/* 헤더 영역 */}
       <Header />
       {/* 할 일을 작성할 edit 영역 */}
-      <TodoEditor />
+      <TodoEditor onCreate={onCreate} />
       {/* 작성한 할 일 목록 영역 */}
       <TodoList />
     </div>
