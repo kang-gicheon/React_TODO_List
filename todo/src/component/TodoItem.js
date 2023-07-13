a24273d4cbc7eda69c49e177596d7c70f5aa4ab1
kanggicheon <snoopy1516@naver.com>
2023-07-13 16:47:40 +0900

    Todoitem 컴포넌트 만들기
    체크박스, 할 일 삭제 버튼 배치 / 임시 렌더링 요소(작성한 할 일, 작성된 시간) 배치

--- todo/src/component/TodoItem.js
+++ todo/src/component/TodoItem.js
@@ -1,1 +1,22 @@
 import "./todoItem.css";
+
+const TodoItem = () => {
+  return (
+    <div className="TodoItem">
+      <div className="checkbox_col">
+        <input type="checkbox" />
+        {/* 할 일 아이템 가장 왼쪽에 할 일 완료 여부를 표시하는 체크박스 배치 */}
+      </div>
+      <div className="title_col">할 일</div>
+      {/* 작성한 할 일 렌더링할 요소 배치 */}
+      <div className="date_col">{new Date().toLocaleDateString()}</div>{" "}
+      {/* 작성된 시간을 렌더링할 요소 배치 */}
+      <div className="btn_col">
+        {/* 할 일 삭제 버튼 배치 */}
+        <button>삭제</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
