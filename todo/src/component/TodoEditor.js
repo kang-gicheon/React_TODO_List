d05c5d9f5a0e1021a3cfdee1245f27647b87776d
kanggicheon <snoopy1516@naver.com>
2023-07-14 16:57:54 +0900

    아이템 추가 함수 호출
    추가 버튼 클릭 ->  app에서 props 받은 함수 onCreate 호출 -> 사용자가 작성한 할 일 인수로 전달

--- todo/src/component/TodoEditor.js
+++ todo/src/component/TodoEditor.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import "./TodoEditor.css";
 
-const TodoEditor = () => {
+const TodoEditor = ({ onCreate }) => {
+  // 사용자가 입력 폼에 입력한 데이터를 저장할 state 생성(변수명: content)
+  const [content, setContent] = useState("");
+  // 입력 폼의 onChange 이벤트 핸들러 onChangeContent 제작
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
   return (
     <div className="TodoEditor">
       <h4>새로운 Todo 작성하기 ✏</h4>
       <div className="editor_wrapper">
-        <input placeholder="새로운 Todo" />
+        {/* 입력 폼의 value 속성을 이용한 content 값, 설정 */}
+        <input
+          value={content}
+          onChange={onChangeContent} // 입력 폼의 value 속성을 이용한 content 값, 설정
+          placeholder="새로운 Todo"
+        />
         <button>추가하기</button>
       </div>
     </div>
