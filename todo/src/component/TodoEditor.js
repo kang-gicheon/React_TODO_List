a149a20cafc1d2c7d37857442271885b0e33e927
kanggicheon <snoopy1516@naver.com>
2023-07-14 17:45:51 +0900

    Create 기능 구현 마무리
    1. 아이템(할 일) 추가 후 입력 폼 자동 초기화
    2. 키보드 Enter키를 누를시 아이템 추가하기
    (크롬 개발자 도구에서 확인하기)

--- todo/src/component/TodoEditor.js
+++ todo/src/component/TodoEditor.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./TodoEditor.css";
 
 const TodoEditor = ({ onCreate }) => {
   // 사용자가 입력 폼에 입력한 데이터를 저장할 state 생성(변수명: content)
   const [content, setContent] = useState("");
+
+  const inputRef = useRef();
 
   // 입력 폼의 onChange 이벤트 핸들러 onChangeContent 제작
   const onChangeContent = (e) => {
@@ -13,16 +15,31 @@ const TodoEditor = ({ onCreate }) => {
   // 추가하기 버튼에 대한 이벤트 핸들러 생성(onSubmit) = onCreate  호출
   // 인수로 content의 값을 전달
   const onsubmit = () => {
+    if (!content) {
+      inputRef.current.focus();
+      return;
+    }
+
     onCreate(content);
+    setContent("");
   };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onsubmit();
+    }
+  };
+
   return (
     <div className="TodoEditor">
       <h4>새로운 Todo 작성하기 ✏</h4>
       <div className="editor_wrapper">
         {/* 입력 폼의 value 속성을 이용한 content 값, 설정 */}
         <input
+          ref={inputRef}
           value={content}
           onChange={onChangeContent} // 입력 폼의 value 속성을 이용한 content 값, 설정
+          onKeyDown={onKeyDown}
           placeholder="새로운 Todo"
         />
         {/* 버튼 클릭 이벤트 핸들러로 함수 onSubmit 설정 */}
