742654329ba824d2eaf1ad0f853c88a276c9de6e
kanggicheon <snoopy1516@naver.com>
2023-07-14 17:04:36 +0900

    추가하기 버튼 클릭 이벤트 핸들러 제작

--- todo/src/component/TodoEditor.js
+++ todo/src/component/TodoEditor.js
@@ -4,11 +4,17 @@ import "./TodoEditor.css";
 const TodoEditor = ({ onCreate }) => {
   // 사용자가 입력 폼에 입력한 데이터를 저장할 state 생성(변수명: content)
   const [content, setContent] = useState("");
+
   // 입력 폼의 onChange 이벤트 핸들러 onChangeContent 제작
   const onChangeContent = (e) => {
     setContent(e.target.value);
   };
 
+  // 추가하기 버튼에 대한 이벤트 핸들러 생성(onSubmit) = onCreate  호출
+  // 인수로 content의 값을 전달
+  const onsubmit = () => {
+    onCreate(content);
+  };
   return (
     <div className="TodoEditor">
       <h4>새로운 Todo 작성하기 ✏</h4>
@@ -19,7 +25,8 @@ const TodoEditor = ({ onCreate }) => {
           onChange={onChangeContent} // 입력 폼의 value 속성을 이용한 content 값, 설정
           placeholder="새로운 Todo"
         />
-        <button>추가하기</button>
+        {/* 버튼 클릭 이벤트 핸들러로 함수 onSubmit 설정 */}
+        <button onClick={onsubmit}>추가하기</button>{" "}
       </div>
     </div>
   );
