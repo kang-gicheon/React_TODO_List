3b0d38e81035ab013d51ac7d964b32bcef379690
kanggicheon <snoopy1516@naver.com>
2023-07-17 09:08:52 +0900

    map함수 응용, HTML 반복
    콜백함수를 이용해 mock데이터 반복, key 생성

--- todo/src/component/TodoItem.js
+++ todo/src/component/TodoItem.js
@@ -1,15 +1,17 @@
 import "./todoItem.css";
 
-const TodoItem = () => {
+const TodoItem = ({ id, content, isDone, createdDate }) => {
   return (
     <div className="TodoItem">
       <div className="checkbox_col">
-        <input type="checkbox" />
+        <input checked={isDone} type="checkbox" />
         {/* 할 일 아이템 가장 왼쪽에 할 일 완료 여부를 표시하는 체크박스 배치 */}
       </div>
-      <div className="title_col">할 일</div>
+      <div className="title_col">{content}</div>
       {/* 작성한 할 일 렌더링할 요소 배치 */}
-      <div className="date_col">{new Date().toLocaleDateString()}</div>{" "}
+      <div className="date_col">
+        {new Date(createdDate).toLocaleDateString()}
+      </div>{" "}
       {/* 작성된 시간을 렌더링할 요소 배치 */}
       <div className="btn_col">
         {/* 할 일 삭제 버튼 배치 */}
