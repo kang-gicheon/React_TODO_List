077e759faa8e63425f07ec19b0e4d72a09151025
kanggicheon <snoopy1516@naver.com>
2023-07-17 16:18:34 +0900

    기능 최종 수정
    동시 선택 오류 수정(app,Item,List) 코드 재 작성
    (최종완성)

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,53 +1,34 @@
-import { useState, useRef } from "react";
 import "./App.css";
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
-
-// 목(Mock) 데이터 설정, 테스트 목적 사용 데이터
+import { useState, useRef } from "react";
 
 const mockTodo = [
   {
     id: 0,
     isDone: false,
     content: "React 공부하기",
-    createDate: new Date().getTime(),
+    createdDate: new Date().getTime(),
   },
-
   {
-    id: 0,
+    id: 1,
     isDone: false,
-    content: "잠자기",
-    createDate: new Date().getTime(),
+    content: "빨래 널기",
+    createdDate: new Date().getTime(),
   },
-
   {
-    id: 0,
+    id: 2,
     isDone: false,
-    content: "아무것도 안 하기",
-    createDate: new Date().getTime(),
+    content: "노래 연습하기",
+    createdDate: new Date().getTime(),
   },
 ];
 
 function App() {
-  // 할 일 아이템 상태를 관리할 state, 배열로 초기화
   const [todo, setTodo] = useState(mockTodo);
-
-  const idRef = useRef(3); // 게시물 테스트 파일에 맞게 초깃값 ref객체 3으로 저장
-
-  const onCreate = (content) => {
-    const newItem = {
-      id: idRef.current, // idRef(현재값 : 3 = 새로 추가할 아이템 id 지정), 처음 추가라면 아이템 id = 3
-      content,
-      isDone: false,
-      createDate: new Date().getTime(),
-    };
-    setTodo([newItem, ...todo]);
-    idRef.current += 1; // idRef의 현재값 + 1, 아이템을 추가할때마다 idRef의 현재값 1씩 증가 (고유 id 갖게함)
-  };
-
+  const idRef = useRef(3);
   const onUpdate = (targetId) => {
-    // 게시물 수정 함수
     setTodo(
       todo.map((it) =>
         it.id === targetId ? { ...it, isDone: !it.isDone } : it
@@ -55,18 +36,25 @@ function App() {
     );
   };
 
+  const onCreate = (content) => {
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createdDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
+
   const onDelete = (targetId) => {
-    // 게시글 삭제 함수
     setTodo(todo.filter((it) => it.id !== targetId));
-  }; // 삭제 버튼 클릭 -> 삭제할 아이템 id 저장, 해당 id 요소를 뺀 새 배열로 todo 업데이트, 대상 아이템 삭제
+  };
 
   return (
     <div className="App">
-      {/* 헤더 영역 */}
       <Header />
-      {/* 할 일을 작성할 edit 영역 */}
       <TodoEditor onCreate={onCreate} />
-      {/* 작성한 할 일 목록 영역 */}
       <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
--- todo/src/component/TodoItem.js
+++ todo/src/component/TodoItem.js
@@ -2,19 +2,19 @@ import "./todoItem.css";
 
 const TodoItem = ({ id, content, isDone, createdDate, onUpdate, onDelete }) => {
   const onChangeCheckbox = () => {
-    // 함수 onUpdate 추가, 체크박스를 클릭시 onChangeCheckbox 함수 호출
+    // 함수 onUpdate, onDelete 추가, 체크박스를 클릭시 onChangeCheckbox 함수 호출
     onUpdate(id); // 인수로 현재 클릭이 발생한 할 일 아이템 id 전달
   };
 
   const onClickDelete = () => {
-    onClickDelete(id);
+    onDelete(id);
   };
 
   return (
     <div className="TodoItem">
       <div className="checkbox_col">
         {/* onChange 이벤트 핸들러를 함수 onChangeCheckbox로 설정 */}
-        <input onChange={onChangeCheckbox} type="checkbox" />
+        <input onChange={onChangeCheckbox} checked={isDone} type="checkbox" />
         {/* 할 일 아이템 가장 왼쪽에 할 일 완료 여부를 표시하는 체크박스 배치 */}
       </div>
       <div className="title_col">{content}</div>
