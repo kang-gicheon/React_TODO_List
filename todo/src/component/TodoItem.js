b41e2568cb98c1ad7aa9e230cec08f720364fbcb
kanggicheon <snoopy1516@naver.com>
2023-07-17 15:03:41 +0900

    Todo 아이템 수정 함수 호출
    Todo Item 컴포넌트에서 이벤트 발생시 onUpdate 호출 기능 제작

--- todo/src/component/TodoItem.js
+++ todo/src/component/TodoItem.js
@@ -1,10 +1,15 @@
 import "./todoItem.css";
 
-const TodoItem = ({ id, content, isDone, createdDate }) => {
+const TodoItem = ({ id, content, isDone, createdDate, onUpdate }) => {
+  const onChangeCheckbox = () => {
+    // 함수 onUpdate 추가, 체크박스를 클릭시 onChangeCheckbox 함수 호출
+    onUpdate(id); // 인수로 현재 클릭이 발생한 할 일 아이템 id 전달
+  };
   return (
     <div className="TodoItem">
       <div className="checkbox_col">
-        <input checked={isDone} type="checkbox" />
+        {/* onChange 이벤트 핸들러를 함수 onChangeCheckbox로 설정 */}
+        <input onChange={onChangeCheckbox} type="checkbox" />
         {/* 할 일 아이템 가장 왼쪽에 할 일 완료 여부를 표시하는 체크박스 배치 */}
       </div>
       <div className="title_col">{content}</div>
